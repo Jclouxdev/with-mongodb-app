478d65e77b6c6ef43ae764828af032f7d11439bc
Joseph Cloux <joseph.clx@gmail.com>
2024-03-07 12:32:48 +0100

    feat(api) New endpoint - Get a movie by ID

--- pages/api/movies/[idMovie].ts
+++ pages/api/movies/[idMovie].ts
@@ -1,20 +1,24 @@
-import {Movie} from "../../types/movie";
+import {Movie} from "../../../types/movie";
 
-// .../api/movies
+// .../api/movies/:id
 export default async function handler(req:any, res:any) {
+    const idMovie = parseInt(req.query.idMovie, 10)
+
     const movies: Movie[] = [
         { _id: 1, title: "The Batman" },
         { _id: 2, title: "The Joker" },
-    ];
+    ]
+
+    const movie = movies.find(({ _id }) => _id === idMovie );
 
     switch (req.method) {
         case "POST":
             // Here the logic for POST case
-            res.json({ status: 200, data: movies, method: "POST" })
+            res.json({ status:200, data: {movie: movie || "not found"}, method: "POST" })
             break;
         case "GET":
             // Here the logic for GET case
-            res.json({ status: 200, data: movies, method: "GET" })
+            res.json({ status:200, data: {movie: movie || "not found"}, method: "GET" })
             break;
     }
 }
